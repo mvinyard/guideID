46876c3878f1c4acc233c66ea1d9d04da0c800ae
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-01-07 12:43:16 -0700

    Update __init__.py

--- sglib/__init__.py
+++ sglib/__init__.py
@@ -1,3 +1,6 @@
 # __init__.py
 
 from ._sgRNA_Library import _sgRNA_Library as Library
+
+# functions
+from ._gene_annotation_functions._merge_reduce_gene_regions import _merge_reduce_gene_regions as merge_reduce
